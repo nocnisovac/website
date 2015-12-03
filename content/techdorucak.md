f6df615579b2f3733a301958b0b1eaa17a04a135
Igor Spasić <igor@heapspace.rs>
2015-12-03 22:36:17 +0100

    tech breakf

---
+++ content/techdorucak.md
@@ -0,0 +1,8 @@
+---
+title: Tech Doručak
+subtitle: Praseće glave s nogicama i čalabrc
+img: hsbck_tekfast.jpg
+kind: tekfast
+created: 2015-12-01
+event: 2015-12-09
+---
--- lib/collections.rb
+++ lib/collections.rb
@@ -2,8 +2,9 @@ require 'erb'
 require 'date'
 
 $alls = Hash.new
+$events = Array.new
 
-
+# collect item times
 def hs_item_times(item)
   if item.key?(:created)
   	print item[:created]
@@ -30,6 +31,18 @@ def hs_item_register(item)
   docs.push(item)
 end
 
+# register events
+def hs_event_register(item)
+  if !item.key?(:event)
+    return
+  end
+
+  $events.push(item)
+  item[:event_at] = attribute_to_time(item[:event])
+
+end
+
+
 # sorts all items by 'created' meta-data
 def hs_sort_alls()
 	$alls.each { |k, v|
@@ -38,7 +51,42 @@ def hs_sort_alls()
 	}
 end
 
+# sort all events
+def hs_sort_events()
+  $events = $events.sort {|left, right| left[:event] < right[:event]}
+end
+
+
 # returns sorted array of items for given kind
 def hs_items_of_kind(kind)
 	return $alls[kind]
+end
+
+def rsmonth(month)
+  case month
+    when 1
+      return "Januar"
+    when 2
+      return "Februar"
+    when 3
+      return "Mart"
+    when 4
+      return "April"
+    when 5
+      return "Maj"
+    when 6
+      return "Jun"
+    when 7
+      return "Jul"
+    when 8
+      return "Avgust"
+    when 9
+      return "Septembar"
+    when 10
+      return "Oktobar"
+    when 11
+      return "Novembar"
+    when 12
+      return "Decembar"
+  end
 end
