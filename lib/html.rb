c56c60934770f40c42e99ee91e8b6faa3cfce638
Igor Spasić <igor@heapspace.rs>
2015-12-07 11:20:21 +0100

    added icons

---
+++ lib/html.rb
@@ -0,0 +1,14 @@
+### SOME HTML SNIPPETS
+
+
+# returns kind icons
+def kind_icon(kind)
+  case kind
+    when 'tekfast'
+      return '<i class="fa fa-coffee"></i>'
+    when 'podkast'
+      return '<i class="fa fa-headphones"></i>'
+    when 'talk'
+      return '<i class="fa fa-comments"></i>'
+  end
+end
